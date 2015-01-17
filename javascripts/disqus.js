042095188b1442a7f28376113100877119228f92
itzhangzh <itzhangzh@sina.com>
2015-01-17 18:56:24 +0800

    Update disqus.js

--- javascripts/disqus.js
+++ javascripts/disqus.js
@@ -2,7 +2,8 @@ $(document).ready(function () {
 	window.disqus_shortname = 'itzhangzh'; // required: replace example with your forum shortname
 	$('#disqus_container .comment').on('click',function(){
 		$(this).html('加载中...');
-		var that = this;
-		$.getScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
+		        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;  
+        dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';  
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);  
 	});
 });
